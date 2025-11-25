94d4e00c39f998119965193d9fae4da0121b64c8
Anastasiia <fohel.anastasiia@student.uzhnu.edu.ua>
2025-11-25 08:01:52 +0000

    Tue, Nov 25, 2025, 10:01 AM +02:00

--- project.py
+++ project.py
@@ -28,6 +28,43 @@ def main():
             print("💙 🇺🇦 💛")
             break
 
+def clean_input(text):
+    return text.lower().strip()
+
+def handle_new_item(products, food_name):
+    create_choise = clean_input(input("There is no such food. Wanna create? y/n "))
+    if create_choise == "y":
+        try:
+            calories = float(input("Enter food calories: "))
+            grams = float(input("Enter product grams/ml: "))
+            products.create_food(food_name, calories, grams)
+            print("Food created ✅")
+        except ValueError:
+            print("⚠️ Invalid input! Need numbers")
+
+
+def get_food_selection(products, results, food_name):
+    print("Found 👇")
+    for index, name, calories in results:
+        print(f"{index}. {name} - {calories} calories/100g")
+
+    while True:
+        try:
+            food_index = int(
+                input("If your dish is not here, enter '0', otherwise enter the dish number: "))
+            food_grams = float(input("Enter product grams/ml: "))
+            products.choose_food(food_index, food_grams)
+
+            if food_index == 0:
+                calories = float(input("Enter food calories: "))
+                products.create_food(food_name, calories, food_grams)
+                print("Food created✅")
+            else:
+                print("Food added✅")
+            break
+        except (IndexError, ValueError):
+            print("⚠️ Invalid number. Please try again ⚠️")
+
 
 if __name__ == "__main__":
     main()
